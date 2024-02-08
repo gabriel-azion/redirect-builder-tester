acc32b96c93cd3e09f1e4dedcb378722a64db11a
gabriel.azion <gabriel.franca@azion.com>
2024-02-08 16:44:33 -0300

    feat: add redirects builder from csv and links  checker

---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type Record struct {
+	From  string `json:"from"`
+	Moved string `json:"moved"`
+}
+
+const (
+	colorRed   = "\033[31m"
+	colorGreen = "\033[32m"
+)
+
+func main() {
+	file, err := os.Open("./json.csv")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = -1
+	rawCSVdata, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var records []Record
+
+	for _, each := range rawCSVdata {
+		record := Record{
+			From:  each[0],
+			Moved: each[1],
+		}
+		records = append(records, record)
+	}
+
+	outputFile, err := os.Create("a.json")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer outputFile.Close()
+
+	encoder := json.NewEncoder(outputFile)
+	if err := encoder.Encode(records); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	TestRedirects()
+}
+
+func TestRedirects() {
+	jsonFile, err := os.Open("a.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer jsonFile.Close()
+
+	byteValue, _ := io.ReadAll(jsonFile)
+
+	var redirects []Record
+	err = json.Unmarshal(byteValue, &redirects)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for i := 0; i < len(redirects); i++ {
+		resp, err := http.Get(redirects[i].Moved)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		if resp.StatusCode == 404 {
+			fmt.Println(colorRed, "Link:", redirects[i].Moved)
+		} else {
+			fmt.Println(colorGreen, "Link:", redirects[i].Moved)
+
+		}
+		resp.Body.Close()
+	}
+}
